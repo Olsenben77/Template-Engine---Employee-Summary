a19ac703918b3a354773a0da9f7366660563d8dc
Ben Olsen <olsenben1992@gmail.com>
2020-02-09 18:18:32 -0800

    added ReadMe

--- Apps.js
+++ Apps.js
@@ -1,54 +1,31 @@
 "use strict";
+const inquirer = require("inquirer");
+const { writeFile } = require("fs");
+const prompts = require("./prompts");
+// async function managerInfo() {
+//   try {
+//     const managerPrompts = await inquirer.prompt([
 
-// module.exports =
-
-// Returns 'Employee
 const Manager = require("./manager");
 const Engineer = require("./engineer");
 const Intern = require("./intern");
 const Employee = require("./employee");
-// const { writeFile } = require("fs");
-// const axios = require("axios");
-// const inquirer = require("inquirer");
-// const { promisify } = require("util");
-// const puppeteer = require("puppeteer");
-// const path = require("path");
-// const open = require("open");
-
-// const writeFileAsync = promisify(writeFile);
-// async function userInfo() {
-//   try {
-//     const answers = await inquirer.prompt([
-//       {
-//         type: "input",
-//         message: "Enter your email address:",
-//         name: "email"
-//       },
-//       {
-//         type: "list",
-//         message: "What is your favorite color?",
-//         choices: ["Red", "Green", "Blue"],
-//         name: "colors"
-//       }
-//     ]);
-//     console.log(answers);
-//     const response = await axios.get(
-//   `//api.github.com/users/${answers.username}`
-// );
-//     const htmltext = html(response.data, answers.colors);
-//     await writeFileAsync("index.html", htmltext, "utf8");
-//     await printPDF();
-//     open("./resume.pdf");
-//     const responseStars = await axios.get(
-//       `https://api.github.com/users/${user.starred}`
-//     );
-//     console.log(responseStars);
-//     const starredtext = html(response.data, user.starred);
-//     await writeFileAsync("index.html", starredtext, "utf8");
-//     await printPDF();
-//     open("./resume.pdf");
-//   } catch (error) {
-//     console.log(error);
-//   }
+async function start() {
+  await managerInfo();
+  console.log("done");
+}
+if (managerPrompts.Confirm === true) {
+  console.log("Let's assemble your team");
+  employeePrompts();
+  return;
+}
+if (managerPrompts.Confirm === false) {
+  console.log("Team is set");
+  return;
+}
+// } catch (error) {
+//   console.log(error);
 // }
-
+start();
+// (async () => {
+//   const response = await {managerPrompts(managerQuestions);
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Template-Engine---Employee-Summary
+# Unit 10 OOP Homework: Template Engine - Employee
+
+## Summary
+
+For our 10th coding camp homework assignment, we had the opportunity to create and develop a functional template engine. This application runs off of Node.js, routing the user through various prompts about their work team. Unique user info is then rendered to an HTML page, displaying the given team and their traits. The Template Engine was created using JavaScript ES6, HTML, and incorporates multiple unit tests to ensure a functional user experience.
+
+## Instructions
+
+To successfully run this application, the user should first clone the repository into a local file and run npm install. Using the local console, the user will next run "node app.js" to trigger the supplied team-creating prompts. Creating a user specific and unique team is done by answering the given questions and inputing information for each team member. Once the team is created, the user will open their local browser find a completed HTML page that includes their team member choices.
+
+![Example](DeployedApplication.png)
+
+## Motivation
+
+Object oriented programming is an important structural base for coding and was used frequently throughout this assignment. I was motivated to learn and use "Class Structure" and "Unit Tests" which were newer concepts this past week. An application like this will be an impressive tool to add to my Portfolio as well as increase my knowledge of OOP.
+
+## Features
+
+Homework 10 features a set of multiple files, working seamlessly together to create a rendered HTML page. Various employee types are created using "Constructor Functions" and their information is exported to an Apps.js file. Specific HTML layouts were created for each employee and these layouts are dynamically generated after a set of user prompts. The application initializes with Node.js and includes inquirer and jest dependencies.
+
+## References:
+
+https://www.w3schools.com/js/js_classes.asp
+
+https://www.npmjs.com/package/inquirer
